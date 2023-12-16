77ba3c4eccc8b537bba5adfe544bda5ad96817a3
BiancaBarcelos <biancassbarcelos@gmail.com>
2023-12-15 22:26:26 -0300

    ajuste html e calculadora no js

--- script.js
+++ script.js
@@ -1,19 +1,58 @@
-function calcularValor(event) {
-
-
-    event.preventDefault();
+function coletaDados() {
     let numero1 = document.getElementById("numero1").value;
     let numero2 = document.getElementById("numero2").value;
-    
+    let operadoresMatematicos = document.getElementById("operadoresMatematicos").value;
 
-    if (numero1 !== "" && numero2 !== ""){
+    const dadosColetados = {
+        num1: numero1,
+        num2: numero2,
+        operador: operadoresMatematicos
+    }
+    return dadosColetados;
+}
 
-        numero1 = parseInt(numero1);
-        numero2 = parseInt(numero2);
+function validaValor(event) {
 
-        document.getElementById("resposta").innerHTML = `Os valores adicionados foram: ${numero1} e ${numero2}`
+    event.preventDefault();
+    let retornaDados = coletaDados();
+    console.log(retornaDados);
+
+    if (retornaDados.num1 !== "" && retornaDados.num2 !== ""){
+
+        document.getElementById("resposta").innerHTML = `A operação selecionada foi ${retornaDados.operador} e os valores adicionados foram ${retornaDados.num1} e ${retornaDados.num2}`;
 
         } else {
+            document.getElementById("resposta").innerHTML = "Os campos obrigatórios não foram preenchidos!";
+        }
+}
+
+function calcularConta(event) {
+
+    event.preventDefault();
+    let retornaDados = coletaDados();
+    let numero1 = retornaDados.num1;
+    let numero2 = retornaDados.num2;
+    let operadoresMatematicos = retornaDados.operador;
+
+    if (numero1 !== "" && numero2 !== ""){
+        numero1 = parseInt(numero1);
+        numero2 = parseInt(numero2);
+        
+        if (operadoresMatematicos === "adição") {
+            valor = numero1 + numero2;
+            document.getElementById("frase").innerHTML = `O resultado da operação ${numero1} + ${numero2} = ${valor}`
+        }else if (operadoresMatematicos === "subtração") {
+            valor = numero1 - numero2;
+            document.getElementById("frase").innerHTML = `O resultado da operação ${numero1} - ${numero2} = ${valor}`
+        }else if (operadoresMatematicos === "divisão") {
+            valor = numero1 / numero2;
+            document.getElementById("frase").innerHTML = `O resultado da operação ${numero1} / ${numero2} = ${valor}`
+
+        }else if (operadoresMatematicos === "multiplicação") {
+            valor = numero1 * numero2;
+            document.getElementById("frase").innerHTML = `O resultado da operação ${numero1} * ${numero2} = ${valor}`
+        }
+    }else {
         alert("Preencha todos os campos indicados!");
     }
 }
